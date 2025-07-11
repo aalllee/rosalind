584e4d9b12337c7145ff56dbb0981bc487a9299c
User <aleksandras.calovas@gmail.com>
2025-07-11 18:23:15 +0300

    Open Reading Frames

---
+++ ORF/main.py
@@ -0,0 +1,71 @@
+
+"""
+Either strand of a DNA double helix can serve as the coding strand for RNA transcription. Hence, a given DNA string implies six total reading frames, or ways in which the same region of DNA can be translated into amino acids: three reading frames result from reading the string itself, whereas three more result from reading its reverse complement.
+
+An open reading frame (ORF) is one which starts from the start codon and ends by stop codon, without any other stop codons in between. Thus, a candidate protein string is derived by translating an open reading frame into amino acids until a stop codon is reached.
+
+Given: A DNA string s of length at most 1 kbp in FASTA format.
+
+Return: Every distinct candidate protein string that can be translated from ORFs of s. Strings can be returned in any order.
+"""
+
+data = "AGCCATGTAGCTAACTCAGGTTACATGGGGATGACCCCGCGACTTGGATTAGAGTCTCTTTTGGAATAAGCCTGAATGATCCGAGTAGCATCTCAG"
+
+def reverse_complement(dna):
+    complement = str.maketrans("ATCG", "TAGC")
+    return dna.translate(complement)[::-1]
+
+
+def translate_orf(orf):
+    codon_table = {
+    'TTT': 'F',     'CTT': 'L',     'ATT': 'I',     'GTT': 'V',
+    'TTC': 'F',     'CTC': 'L',     'ATC': 'I',     'GTC': 'V',
+    'TTA': 'L',     'CTA': 'L',     'ATA': 'I',     'GTA': 'V',
+    'TTG': 'L',     'CTG': 'L',     'ATG': 'M',     'GTG': 'V',
+    'TCT': 'S',     'CCT': 'P',     'ACT': 'T',     'GCT': 'A',
+    'TCC': 'S',     'CCC': 'P',     'ACC': 'T',     'GCC': 'A',
+    'TCA': 'S',     'CCA': 'P',     'ACA': 'T',     'GCA': 'A',
+    'TCG': 'S',     'CCG': 'P',     'ACG': 'T',     'GCG': 'A',
+    'TAT': 'Y',     'CAT': 'H',     'AAT': 'N',     'GAT': 'D',
+    'TAC': 'Y',     'CAC': 'H',     'AAC': 'N',     'GAC': 'D',
+    'TAA': 'Stop',  'CAA': 'Q',     'AAA': 'K',     'GAA': 'E',
+    'TAG': 'Stop',  'CAG': 'Q',     'AAG': 'K',     'GAG': 'E',
+    'TGT': 'C',     'CGT': 'R',     'AGT': 'S',     'GGT': 'G',
+    'TGC': 'C',     'CGC': 'R',     'AGC': 'S',     'GGC': 'G',
+    'TGA': 'Stop',  'CGA': 'R',     'AGA': 'R',     'GGA': 'G',
+    'TGG': 'W',     'CGG': 'R',     'AGG': 'R',     'GGG': 'G'
+}
+
+    protein = []
+    for i in range(0,len(orf),3):
+        protein.append(codon_table[orf[i:i+3]]) 
+
+    return "".join(protein)
+
+def find_orfs(dna):
+    orfs = set()
+
+    data = [dna, reverse_complement(dna)]
+
+    for j in range(3):
+        for s in data:
+            start = []
+            stop = []
+            for i in range(0,len(s)-2-j,3):
+                if s[i+j:i+j+3]=="ATG":
+                    start.append(i+j)
+                elif s[i+j:i+j+3] in ["TAA","TAG","TGA"]:
+                    stop.append(i+j)
+
+            for i in start:
+                for j in stop:
+                    if i < j:
+                        orfs.add(s[i:j])
+                        break
+    return orfs
+
+
+orfs = find_orfs(data)   
+
+for orf in orfs:
+    print(translate_orf(orf))
