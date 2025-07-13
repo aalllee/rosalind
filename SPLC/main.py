8f8b024d04043c1e261a5037d648ca735f08f940
User <aleksandras.calovas@gmail.com>
2025-07-13 15:49:45 +0300

    splicing RNA

---
+++ SPLC/main.py
@@ -0,0 +1,59 @@
+
+"""
+After identifying the exons and introns of an RNA string, we only need to delete the introns and concatenate the exons to form a new string ready for translation.
+
+Given: A DNA string s (of length at most 1 kbp) and a collection of substrings of s acting as introns. All strings are given in FASTA format.
+
+Return: A protein string resulting from transcribing and translating the exons of s. (Note: Only one solution will exist for the dataset provided.)
+
+"""
+
+
+dna = "ATGGTCTACATAGCTGACAAACAGCACGTAGCAATCGGTCGAATCTCGAGAGGCATATGGTCACATGATCGGTCGAGCGTGTTTCAAAGTTTGCGCCTAG"
+
+introns = ["ATCGGTCGAA","ATCGGTCGAGCGTGT"]
+
+def dna_splice(dna, introns):
+    exon = dna
+    sorted(introns,key=len,reverse=True)
+    for intron in introns:
+        exon = exon.replace(intron,"")
+
+    return exon
+
+
+def dna_to_protein(dna):
+    codon_table = {
+        'ATA':'I', 'ATC':'I', 'ATT':'I', 'ATG':'M',
+        'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACT':'T',
+        'AAC':'N', 'AAT':'N', 'AAA':'K', 'AAG':'K',
+        'AGC':'S', 'AGT':'S', 'AGA':'R', 'AGG':'R',
+        'CTA':'L', 'CTC':'L', 'CTG':'L', 'CTT':'L',
+        'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCT':'P',
+        'CAC':'H', 'CAT':'H', 'CAA':'Q', 'CAG':'Q',
+        'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGT':'R',
+        'GTA':'V', 'GTC':'V', 'GTG':'V', 'GTT':'V',
+        'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCT':'A',
+        'GAC':'D', 'GAT':'D', 'GAA':'E', 'GAG':'E',
+        'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGT':'G',
+        'TCA':'S', 'TCC':'S', 'TCG':'S', 'TCT':'S',
+        'TTC':'F', 'TTT':'F', 'TTA':'L', 'TTG':'L',
+        'TAC':'Y', 'TAT':'Y', 'TAA':'_', 'TAG':'_',
+        'TGC':'C', 'TGT':'C', 'TGA':'_', 'TGG':'W',
+    }
+
+    protein = ''
+    dna = dna.upper()
+    for i in range(0, len(dna) - 2, 3): 
+        codon = dna[i:i+3]
+        aa = codon_table.get(codon, '')
+        if aa == '_': 
+            break
+        protein += aa
+    return protein
+
+
+exon_chain = dna_splice(dna,introns)
+
+protein = dna_to_protein(exon_chain)
+print(protein)
